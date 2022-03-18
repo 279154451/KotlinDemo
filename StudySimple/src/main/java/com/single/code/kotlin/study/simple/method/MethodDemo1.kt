3aee5c7819cb4eceec2a647c02fff9c178caf49e
CZF <279154451@qq.com>
2022-03-18 19:32:52 +0800

    kotlin 最简单的高阶函数

--- StudySimple/src/main/java/com/single/code/kotlin/study/simple/method/MethodDemo1.kt
+++ StudySimple/src/main/java/com/single/code/kotlin/study/simple/method/MethodDemo1.kt
@@ -8,6 +8,7 @@ package com.single.code.kotlin.study.simple.method
 fun main() {
     show(true){
         println("loginSuccess=$it")
+        "已经获得结果了=$it"
     }
 }
 //TODO callBackMethod:(Boolean)->Unit
@@ -15,12 +16,16 @@ fun main() {
 //(Boolean) 方法的（），方法的参数类型是Boolean
 //->方法体
 //Unit方法的返回值 Unit==void
-fun show(isLogin:Boolean,callBackMethod:(Boolean)->Unit){
+fun show(isLogin:Boolean,callBackMethod:(Boolean)->String){
     if(isLogin){
         println("登录成功")
-        callBackMethod(true)//调用函数
+       // 调用函数
+        var result = callBackMethod.invoke(true)
+        println("函数返回=$result")
+//        callBackMethod(true)//这样也可以
     }else{
         println("登录失败")
+        callBackMethod.invoke(false)
         callBackMethod(false)
     }
 }
